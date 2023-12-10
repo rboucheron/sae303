f3bffc19be62580cd3e4cca6b27ef690773d4c45
Raphaël <115149179+rboucheron@users.noreply.github.com>
2023-12-10 19:26:37 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.php", "./connexion.php", "./inscription.php", "./profil.php"],
+  content: ["./index.php", "./connexion.php", "./inscription.php", "./profil.php", "./component/header.php"],
   theme: {
     extend: {},
   },
