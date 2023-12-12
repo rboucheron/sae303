262a2332cc0cf635a8aab6fd60ae8a3261550d11
jysan <sanjy.boot@gmail.com>
2023-12-12 17:48:17 +0100

    début page forfait

---
+++ forfait.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./dist/style.css">
+    <title>Sélection de Forfait</title>
+</head>
+
+<body>
+<?php include('./component/header.php')?> 
+<section class="w-full mt-36 ">
+<div class="max-w-md  m-24 rounded overflow-hidden shadow-lg">
+      <div class="px-6 py-4">
+        <h2 class="font-bold text-2xl mb-2">This is My Blog Title</h2>
+        <p class="mt-3 text-gray-600 text-base">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+        </p>
+        <button class="mt-4 bg-blue-500 text-white font-bold py-2 px-4 rounded">
+        <div class=" grid"> <a class=" bg-cyan-500 w-full text-center rounded-xl textfont-semibold text-xl cursor-pointer  p-3 " href="catalogue.php">GO</a></div>
+        </button>
+      </div>
+    </div>
+</body>
+</html>
