a073f8b9d054c1cd73fd9acbed96dc52e6a934eb
Raphaël <115149179+rboucheron@users.noreply.github.com>
2023-12-13 09:33:07 +0100

    update api

--- js/allModal.js
+++ js/allModal.js
@@ -1,10 +1,8 @@
 var x = 0, y = 0;
-
 window.addEventListener('scroll', function() {
     x++;
     y++;
 });
-
 function openForm() {
     var form = document.getElementById('updateForm');
     form.classList.remove('hidden');
@@ -31,11 +29,9 @@ function closeMenu(){
     document.body.style.overflow = "scroll"; 
 
 }
-
-
 async function afficheMeteo(){
     try {
-        const response = await fetch('http://api.openweathermap.org/data/2.5/forecast?q=Paris&appid=8886a19c93163d930ae4268518360a0&units=metric";');
+        const response = await fetch('https://api.openweathermap.org/data/2.5/forecast?q=Paris&appid=78886a19c93163d930ae4268518360a0&units=metric%22');
         const text = await response.text();
         console.log(text);
       } catch (error) {
