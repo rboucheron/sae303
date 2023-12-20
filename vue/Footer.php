1897ae469b8dbcebd0b027483d43a11d99a19709
JarafG <jaraf2004@gmail.com>
2023-12-20 16:09:54 +0100

    carroussel modif

--- vue/Footer.php
+++ vue/Footer.php
@@ -1,5 +1,6 @@
     <script src="./js/allModal.js"></script>
     <script src="./js/Carrousel.js"></script>
+    <script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>
 </body>
 
 </html>
