b4c0c29282cbd70d8efb9161aa7ff921e9dc3c3c
Raphaël <115149179+rboucheron@users.noreply.github.com>
2023-12-23 09:46:44 +0100

    ajouts mois

--- vue/Reservation.php
+++ vue/Reservation.php
@@ -23,53 +23,34 @@ if (isset($_GET['plane'])) {
 
         <div class="w-3/4 m-auto  p-2">
             <h2 class="text-5xl text-sky-800 w-full text-center">Choisir la date</h2>
-            <div class="w-2/5 m-auto ">
+            <div class="w-2/5 m-auto grid grid-cols-3 mt-10 mb-10">
+                <div>
+                    <div class="bg-gray-200 p-2 rounded-xl place-items-end w-1/2 m-auto cursor-pointer hover:bg-sky-200" onclick="RemoveMonth()">
 
-                <h3 class="text-2xl text-gray-800 text-center mt-10 mb-10" id="month"></h3>
-           
-
+                        <img src="./assets/images/chevron-left.svg" alt="right">
+                    </div>
+                </div>
+                <h3 class="text-2xl text-gray-800 text-center " id="month"></h3>
+                <div>
+                    <div class="bg-gray-200 p-2 rounded-xl place-items-end w-1/2 m-auto cursor-pointer hover:bg-sky-200" onclick="AddMonth()">
+                        <img src="./assets/images/chevron-right.svg" alt="right">
+                    </div>
+                </div>
             </div>
 
             <div class="grid grid-cols-7 mt-5">
-                <div class="border border-white  text-start p-2">
-                    Dimanche
-
-                </div>
-                <div class="border border-white text-start p-2 ">
-                    Lundi
-
-                </div>
-                <div class="border border-white  text-start p-2">
-                    Mardi
-
-                </div>
-                <div class="border border-white   text-start p-2">
-                    Mercredi
-
-                </div>
-                <div class="border border-white  text-start p-2">
-                    Jeudi
-
-                </div>
-                <div class="border border-white  text-start p-2">
-                    Vendredi
-
-                </div>
-                <div class="border border-white  text-start p-2">
-                    Samedi
-
-                </div>
+                <div class="border border-white text-start p-2">Dimanche</div>
+                <div class="border border-white text-start p-2 ">Lundi</div>
+                <div class="border border-white text-start p-2">Mardi</div>
+                <div class="border border-white text-start p-2">Mercredi</div>
+                <div class="border border-white text-start p-2">Jeudi</div>
+                <div class="border border-white text-start p-2">Vendredi</div>
+                <div class="border border-white text-start p-2">Samedi</div>
             </div>
-            <div class="grid grid-cols-7 mt-5" id="calendar">
-            </div>
-
-
-
+            <div class="grid grid-cols-7 mt-5" id="calendar"></div>
         </div>
-
     </div>
 
-    </div>
 
 </section>
 <script>
@@ -88,15 +69,35 @@ if (isset($_GET['plane'])) {
         'Decembre',
     ];
     var today = new Date();
-    var firstDay = new Date(today.getFullYear(), today.getMonth(), 1);
-    var lastDay = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+    var month = today.getMonth();
+    var year = today.getFullYear();
+    var firstDay;
+    var lastDay;
+    Give();
     var div = "<div class='border border-white p-2'></div>";
     var calendar = div;
     Fill();
     Write();
+
     function AddMonth() {
-        firstDay = new Date(today.getFullYear(), today.getMonth() + 1, 1);
-        lastDay = new Date(today.getFullYear(), today.getMonth() + 2, 0);
+        month++;
+        if (month > 11) {
+            month = 0;
+            year++;
+        }
+        Give();
+        calendar = div;
+        Fill();
+        Write();
+    }
+
+    function RemoveMonth() {
+        month--;
+        if (month < 0) {
+            month = 11;
+            year--;
+        }
+        Give();
         calendar = div;
         Fill();
         Write();
@@ -108,17 +109,25 @@ if (isset($_GET['plane'])) {
             }
         }
         var Classe;
-        for (let i = firstDay.getDate(); i <= lastDay.getDate(); i++) {
-            if ((i < today.getDate())&&(firstDay.getMonth < today.getMonth)) {
+        for (let i = 1; i <= lastDay.getDate(); i++) {
+            if ((firstDay.getMonth() < today.getMonth()) && (firstDay.getFullYear() <= today.getFullYear())) {
                 Classe = "border border-sky-500 text-start p-2 pb-10 bg-gray-200";
             } else {
-                Classe = "border border-sky-500 text-start p-2 pb-10 cursor-pointer hover:bg-sky-200";
+                if ((firstDay.getMonth() == today.getMonth()) && (firstDay.getDate() < today.getDate())) {
+                    Classe = "border border-sky-500 text-start p-2 pb-10 bg-gray-200";
+                } else {
+                    Classe = "border border-sky-500 text-start p-2 pb-10 cursor-pointer hover:bg-sky-200";
+                }
             }
-            calendar += "<div class='" + Classe + "' id='" + i + "/" + today.getMonth() + "'>" + i + "</div>";
+            calendar += "<div class='" + Classe + "' id='" + i + "/" + firstDay.getMonth() + "'>" + i + "</div>";
         }
     }
     function Write() {
         document.getElementById('month').innerText = months[firstDay.getMonth()];
         document.getElementById('calendar').innerHTML = calendar;
+    }
+    function Give() {
+        firstDay = new Date(year, month, 1);
+        lastDay = new Date(year, month + 1, 0);
     }
 </script>
