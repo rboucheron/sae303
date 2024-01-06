b6f3f9a4ea83a65e441e5b2862eff716f2381cc6
darryl-sangare <darryl.sangare@gmail.com>
2024-01-07 00:33:52 +0100

    change accueil

--- vue/Accueil.php
+++ vue/Accueil.php
@@ -1,8 +1,14 @@
-<section class="bg-gradient-to-r from-cyan-200 to-blue-500 w-full  p-14 ">
+<section class="bg-gradient-to-r from-cyan-200 to-blue-500 w-full relative  p-14 ">
+    <div class="w-48 shadow-xl border-gray-500 bg-white rounded-r-xl absolute top-53 left-0" >
+        <img class="rounded-r-xl shadow-xl   border-gray-500" src="./assets/images/PipistrelSinus.jpeg" alt="">
+    </div>
         <h1 class="text-6xl lg:text-8xl w-3/4 m-auto text-center px-6 text-gray-50 font-bold ">VENEZ PILOTER <br>UN ULM</h1>
         <p class="mt-2 text-2xl w-1/3 m-auto text-center text-slate-700 font-semibold ">BIENVENUE CHEZ AC2FL</p>
         <div class="w-1/2 lg:w-1/3 m-auto mt-9 cursor-pointer">
             <div class="rounded-full w-20 m-auto bg-blue-700 hover:bg-blue-600 duration-300  hover:scale-105  hover:-translate-y-1 hover:shadow-lg"><img src="./assets/images/arrow.svg" class="w-24 m-auto  " alt=""></div>
 
         </div>
+        <div class="w-48 shadow-xl	 bg-white rounded-tl-xl absolute bottom-0 right-0" >
+        <img class="rounded-tl-xl shadow-xl	" src="./assets/images/b.jpg" alt="">
+    </div>
 </section>
