4927e6b75590b80008bdcbab13f14bf48257494f
darryl-sangare <darryl.sangare@gmail.com>
2024-01-07 18:18:03 +0100

    change accueil

--- vue/Accueil.php
+++ vue/Accueil.php
@@ -4,10 +4,7 @@
     </div>
         <h1 class="text-6xl lg:text-8xl w-3/4 m-auto text-center px-6 text-gray-50 font-bold ">VENEZ PILOTER <br>UN ULM</h1>
         <p class="mt-2 text-2xl w-1/3 m-auto text-center text-slate-700 font-semibold ">BIENVENUE CHEZ AC2FL</p>
-        <div class="w-1/2 lg:w-1/3 m-auto mt-9 cursor-pointer">
-            <div class="rounded-full w-20 m-auto bg-blue-700 hover:bg-blue-600 duration-300  hover:scale-105  hover:-translate-y-1 hover:shadow-lg"><img src="./assets/images/arrow.svg" class="w-24 m-auto  " alt=""></div>
 
-        </div>
         <div class="w-48 shadow-xl	 bg-white rounded-tl-xl absolute bottom-0 right-0" >
         <img class="hidden lg:grid rounded-tl-xl shadow-xl	" src="./assets/images/b.jpg" alt="">
     </div>
